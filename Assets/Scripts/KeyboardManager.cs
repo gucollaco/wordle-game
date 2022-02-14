f5502844cfa8422eb33680d937a48cf4635cec1f
Gustavo Collaco <gustavo.m.collaco@gmail.com>
2022-02-14 15:07:20 -0300

    feat: keyboard manager setup

---
+++ Assets/Scripts/KeyboardManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeyboardManager : MonoBehaviour
+{
+    private GameObject keyboard;
+    private string[] characters;
+
+    void Start()
+    {
+        InitializeCharacterList();
+
+        keyboard = GameObject.Find("Keyboard");
+
+        // Go through each keyboard row.
+        for(int i = 0; i < keyboard.transform.childCount; i++)
+        {
+            Transform row = keyboard.transform.GetChild(i);
+
+            // Backspace and enter buttons.
+            if (i == 0)
+            {
+                Transform backspace = row.transform.GetChild(0);
+                Button backspaceButton = backspace.GetComponent<Button>();
+                backspaceButton.onClick.AddListener(BackspaceKeyClick);
+
+                Transform confirm = row.transform.GetChild(1);
+                Button confirmButton = confirm.GetComponent<Button>();
+                confirmButton.onClick.AddListener(ConfirmKeyClick);
+            }
+            // Character buttons.
+            else
+            {
+                // Setting up the text content for each keyboard character button.
+                for (int j = 0; j < row.transform.childCount; j++)
+                {
+                    Transform letter = row.transform.GetChild(j);
+                    TextMeshProUGUI letterText = letter.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    string text = characters[i-1][j].ToString();
+                    letterText.text = text;
+                    Button letterButton = letter.GetComponent<Button>();
+                    letterButton.onClick.AddListener(() => CharacterKeyClick(text));
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // Initializing the characters string array.
+    private void InitializeCharacterList()
+    {
+        characters = new string[3];
+        
+        characters[0] = "QWERTYUIOP";
+        characters[1] = "ASDFGHJKL";
+        characters[2] = "ZXCVBNM";
+    }
+
+    private void CharacterKeyClick(string letter)
+    {
+        Debug.Log(letter);
+    }
+
+    private void BackspaceKeyClick()
+    {
+        Debug.Log("Backspace clicked");
+    }
+
+    private void ConfirmKeyClick()
+    {
+        Debug.Log("Confirm clicked");
+    }
+}
